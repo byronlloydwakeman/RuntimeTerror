8340bd0aef7d1822d4affb6cc0a4d2a9f2f9fa29
Byron Lloyd Wakeman <byzstorm1103@gmail.com>
2024-04-09 12:16:35 +0100

    Created custom input

---
+++ components/WeatherApp/CustomTextInput.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import styles from "./customtextinput.module.scss";
+
+export const CustomTextInput = ({value, onChange}) => {
+    return (
+        <div id="weatherInput" className={styles.custom_text_input__container}>
+            <input id="inputForWeather" value={value} onChange={onChange} onBlur={() => {
+                var element = document.getElementById("weatherInput")
+                element.classList.remove(`${styles.custom_text_input__boxSlideInComplete}`);
+            }} 
+            onFocus={() => {
+                var element = document.getElementById("weatherInput");
+                element.classList.add(`${styles.custom_text_input__boxSlideInComplete}`);
+            }}
+            className={styles.custom_text_input} type="text" placeholder="🔎︎ Search"/>
+        </div>
+    )
+}
