3cae6a325754ed9bc13f5bdbb7c99888ddd0079a
tharanpatel <tharan451@gmail.com>
2024-04-25 14:08:27 +0100

    add winning logic and minor styling

--- pages/challenges/connectfour.jsx
+++ pages/challenges/connectfour.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import styles from './connectfour.module.scss'; // Importing SCSS module
+import { array } from '@google/maps/lib/internal/validate';
+import Navbar from '../../components/Navbars/Navbar';
+import NavbarBottom from '../../components/Navbars/NavbarBottom';
+import { Button } from '@mui/material';
 
 const COLUMN_COUNT = 7;
 const ROW_COUNT = 6;
@@ -14,6 +18,10 @@ const ConnectFour = () => {
   const [discColors, setDiscColors] = useState(
     Array.from({ length: COLUMN_COUNT }, () => new Array(ROW_COUNT).fill(null))
   );
+  const [player1WinCount, setPlayer1WinCount] = useState(0);
+  const [player2WinCount, setPlayer2WinCount] = useState(0);
+  const [gameState, setGameState] = useState('Match in progress...');
+
   const discAnimationControls = useAnimation(); // Animation controls for the disc
 
   const handleColumnClick = async (col) => {
@@ -33,7 +41,7 @@ const ConnectFour = () => {
       setPlayer1Go(!player1Go); // Toggle player's turn
     }
 
-    horizontalWinCheck(board);
+    checkWin(board);
   };
 
   const horizontalWinCheck = (array) => {
@@ -41,59 +49,147 @@ const ConnectFour = () => {
       for (let j = 0; j < 5; j++)
         if (
           array[i][j] == array[i + 1][j] &&
-          array[i + 1][j] == array[i + 2][j] &&
-          array[i + 2][j] == array[i + 3][j] &&
+          array[i][j] == array[i + 2][j] &&
+          array[i][j] == array[i + 3][j] &&
           array[i][j]
         ) {
-          console.log('winner');
+          playerWinLogic(player1Go);
         }
     }
   };
 
   const verticalWinCheck = (array) => {
-    for (let i = 0; i < array.length - 3; i++) {
-      for (let j = 0; j < 5; j++)
+    for (let i = 0; i < array.length - 1; i++) {
+      for (let j = 0; j < 2; j++)
         if (
-          array[i][j] == array[i + 1][j] &&
-          array[i + 1][j] == array[i + 2][j] &&
-          array[i + 2][j] == array[i + 3][j] &&
+          array[i][j] == array[i][j + 1] &&
+          array[i][j] == array[i][j + 2] &&
+          array[i][j] == array[i][j + 3] &&
           array[i][j]
         ) {
-          console.log('winner');
+          playerWinLogic(player1Go);
         }
     }
   };
 
+  const diagonalWinCheckAsc = (array) => {
+    for (let i = 0; i < array.length - 3; i++) {
+      for (let j = 0; j < 2; j++)
+        if (
+          array[i][j] == array[i + 1][j + 1] &&
+          array[i][j] == array[i + 2][j + 2] &&
+          array[i][j] == array[i + 3][j + 3] &&
+          array[i][j]
+        ) {
+          playerWinLogic(player1Go);
+        }
+    }
+  };
+
+  const diagonalWinCheckDesc = (array) => {
+    for (let i = 0; i < array.length - 3; i++) {
+      for (let j = 5; j >= 3; j--)
+        if (
+          array[i][j] == array[i + 1][j - 1] &&
+          array[i][j] == array[i + 2][j - 2] &&
+          array[i][j] == array[i + 3][j - 3] &&
+          array[i][j]
+        ) {
+          playerWinLogic(player1Go);
+        }
+    }
+  };
+
+  const playerWinLogic = (player1Go) => {
+    if (player1Go) {
+      setPlayer1WinCount(player1WinCount + 1);
+      setGameState('Player 1 wins!');
+    } else {
+      setPlayer2WinCount(player2WinCount + 1);
+      setGameState('Player 2 wins!');
+    }
+    setTimeout(() => {
+      resetBoard();
+    }, 1000);
+  };
+
+  const resetBoard = () => {
+    setBoard(
+      Array.from({ length: COLUMN_COUNT }, () =>
+        new Array(ROW_COUNT).fill(null)
+      )
+    );
+    setGameState('Match in progress...');
+  };
+
+  const checkWin = (array) => {
+    horizontalWinCheck(array);
+    verticalWinCheck(array);
+    diagonalWinCheckAsc(array);
+    diagonalWinCheckDesc(array);
+  };
+
   console.log(board);
 
+  const buttonStyle = {
+    Button: {
+      color: '#045149',
+      border: '1px solid #045149',
+      backgroundColor: 'white',
+      padding: '5px 10px',
+      '&:hover': {
+        backgroundColor: '#045149 !important',
+        boxShadow: 'none !important',
+        borderColor: 'white',
+        color: 'white',
+      },
+    },
+  };
+
   return (
-    <div className={styles.board}>
-      {board.map((column, colIndex) => (
-        <div
-          key={colIndex}
-          className={styles.column}
-          onClick={async () => {
-            await handleColumnClick(colIndex);
-            await discAnimationControls.start({
-              y: 0,
-              transition: { duration: 1 },
-            });
-          }}
-        >
-          {column.map((cell, rowIndex) => (
-            <div key={rowIndex} className={styles.cell}>
-              {cell && (
-                <motion.div
-                  className={`${styles.disc} ${discColors[colIndex][rowIndex]}`} // Use discColors to determine the color class
-                  initial={{ y: -CELL_SIZE * ROW_COUNT }} // Start disc above the board
-                  animate={discAnimationControls} // Use animation controls
-                  onAnimationComplete={() => {}}
-                />
-              )}
-            </div>
-          ))}
+    <div>
+      <div>
+        <Navbar />
+        <div className={styles.mainContainer}>
+          <div className={styles.board}>
+            {board.map((column, colIndex) => (
+              <div
+                key={colIndex}
+                className={styles.column}
+                onClick={async () => {
+                  await handleColumnClick(colIndex);
+                  await discAnimationControls.start({
+                    y: 0,
+                    transition: { duration: 1 },
+                  });
+                }}
+              >
+                {column.map((cell, rowIndex) => (
+                  <div key={rowIndex} className={styles.cell}>
+                    {cell && (
+                      <motion.div
+                        className={`${styles.disc} ${discColors[colIndex][rowIndex]}`} // Use discColors to determine the color class
+                        initial={{ y: -CELL_SIZE * ROW_COUNT }} // Start disc above the board
+                        animate={discAnimationControls} // Use animation controls
+                        onAnimationComplete={() => {}}
+                      />
+                    )}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+          <div className={styles.gameInformation}>
+            <p>{gameState}</p>
+            <p>Player 1: {player1WinCount}</p>
+            <p>Player 2: {player2WinCount}</p>
+            <Button size="small" sx={buttonStyle.Button} onClick={resetBoard}>
+              Reset Board
+            </Button>
+          </div>
         </div>
-      ))}
+      </div>
+      <NavbarBottom />
     </div>
   );
 };
